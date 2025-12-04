50f19f8365fea3e84f8bf38047be3563a5f524c5
s-aaahill <sahilshaikh2662@gmail.com>
2025-12-04 19:46:14 +0530

    Implemented PIC Driver and IRQ Handling

--- src/kernel/arch/i686/io.asm
+++ src/kernel/arch/i686/io.asm
@@ -1,14 +1,14 @@
 
-global x86_outb
-x86_outb:
+global i686_outb
+i686_outb:
     [bits 32]
     mov dx, [esp + 4]
     mov al, [esp + 8]
     out dx, al
     ret
 
-global x86_inb
-x86_inb:
+global i686_inb
+i686_inb:
     [bits 32]
     mov dx, [esp + 4]
     xor eax, eax
@@ -19,6 +19,16 @@ global i686_Panic
 i686_Panic:
     cli
     hlt
+
+global i686_EnableInterrupts
+i686_EnableInterrupts:
+    sti
+    ret
+
+global i686_DisableInterrupts
+i686_DisableInterrupts:
+    cli
+    ret
 
 global crash_me
 crash_me:
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,11 +2,17 @@
 #include "stdio.h"
 #include "memory.h"
 #include <hal/hal.h>
+#include <arch/i686/irq.h>
 
 extern uint8_t __bss_start;
 extern uint8_t __end;
 
 void crash_me();
+
+void timer(Registers* regs) {
+
+    printf(".");
+}
 
 void __attribute__((section(".entry"))) start(uint16_t bootDrive)
 {
@@ -18,7 +24,9 @@ void __attribute__((section(".entry"))) start(uint16_t bootDrive)
 
     printf("Control Flow in Kernel\nFloppyOS under development!!!\nCompleted till here on 4th December 2025\n");
 
-    crash_me();
+    i686_IRQ_RegisterHandler(0, timer);
+
+    //crash_me();
 
 end:
     for (;;);
